4d6db1a1730c44f563effe69b1443a2164fcf38d
Rafael Acioly <aciolyr@gmail.com>
2020-09-09 12:27:48 -0300

    adds gorilla mux

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/rafa-acioly/zip-finder
 
 go 1.15
+
+require github.com/gorilla/mux v1.8.0
--- main.go
+++ main.go
@@ -8,26 +8,21 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/gorilla/mux"
 	"github.com/rafa-acioly/zip-finder/core"
 	"github.com/rafa-acioly/zip-finder/services"
 )
 
 func main() {
-	http.HandleFunc("/", handler)
-	fmt.Println("Listening on port 8080...")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	router := mux.NewRouter()
+	router.HandleFunc("/{zipcode}", handler).Methods(http.MethodGet)
+	router.Use(mux.CORSMethodMiddleware(router))
+
+	fmt.Println("Listening on port: 8080")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		http.Error(
-			w,
-			http.StatusText(http.StatusMethodNotAllowed),
-			http.StatusMethodNotAllowed,
-		)
-		return
-	}
-
 	ctx, cancelCtx := context.WithTimeout(r.Context(), time.Second*1)
 	defer cancelCtx()
 
@@ -35,11 +30,12 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		&services.ViaCep{},
 		&services.PostMon{},
 	}
-	channel := make(chan core.ServiceResponse, len(zipCodeServices))
 
+	channel := make(chan core.ServiceResponse, len(zipCodeServices))
+	vars := mux.Vars(r)
 	baseService := services.BaseService{}
 	for _, service := range zipCodeServices {
-		go baseService.DoRequest("07400885", service, channel)
+		go baseService.DoRequest(vars["zipcode"], service, channel)
 	}
 
 	select {
